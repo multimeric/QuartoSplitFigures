e0605ea12254017e9c96d8baf9d3c2bbb1b05502
Michael Milton <michael.r.milton@gmail.com>
2023-09-08 01:23:52 +1000

    Remove logging call

--- _extensions/split-figures/split-figures.lua
+++ _extensions/split-figures/split-figures.lua
@@ -1,7 +1,6 @@
 function Div(div)
   if div.identifier:sub(0, 3) == "fig" then
     file = assert(io.open(div.identifier .. "." .. FORMAT, "w"))
-    quarto.log.output(div)
     file:write(
       pandoc.write(
         pandoc.Pandoc(div), FORMAT, PANDOC_WRITER_OPTIONS
